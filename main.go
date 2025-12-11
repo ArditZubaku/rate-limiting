4923910709fea4c5e6211acc73bb6e222468eaa0
ArditZubaku <zubaku92@gmail.com>
2025-12-11 10:58:28 +0100

    add server with rate limiting middleware

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"sync"
+
+	"github.com/ArditZubaku/rate-limiting/limiter"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write([]byte("hi there\n"))
+	})
+
+	handler := rateLimiterMiddleware(mux, limiter.NewSlidingWindow(1, 2)) // 2 reqs per second
+
+	err := http.ListenAndServe("127.0.0.1:8080", handler)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func rateLimiterMiddleware(
+	next http.Handler,
+	rateLimiter limiter.RateLimiter,
+) http.Handler {
+	// For each ip/client we will a rate limiter
+	ipToLimiterMap := sync.Map{}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get the IP of the client
+		ip := getClientIP(r)
+
+		// Rate limit
+		ipLimiter, _ := ipToLimiterMap.LoadOrStore(ip, rateLimiter)
+
+		if !ipLimiter.(limiter.RateLimiter).Allow() {
+			w.WriteHeader(http.StatusTooManyRequests)
+			return
+		}
+
+		// ipToLimiterMap[ip] = ipLimiter
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getClientIP(r *http.Request) string {
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		log.Println("Failed to get client IP:", err)
+	}
+
+	return host
+}
